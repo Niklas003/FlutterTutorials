6e4c61a3d507d49bf59b28dc196db99b771d8db6
Niklas <62644517+Niklas003@users.noreply.github.com>
2023-01-15 16:00:20 +0100

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,25 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Das ist der Text, welcher in der Mitte steht und etwas über die ganze Sache aussagen soll. Peng oder was hahah. Lol ich hab ewcht keine Ahnung was hier hinensoll. BULLSHIT schreit der Adler als er sieht dass der Bauer seinen Wagen mal wieder in das Schwimmbecken des örtlichen Freibads lenkt. ENDE',
+        softWrap: true,
+      ),
+    );
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
@@ -53,9 +72,32 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
