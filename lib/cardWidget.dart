be2ecfdacf502824b57a5d66130d398dee24a3c1
Niklas <62644517+Niklas003@users.noreply.github.com>
2023-01-16 20:45:04 +0100

    working

--- lib/cardWidget.dart
+++ lib/cardWidget.dart
@@ -1,32 +1,15 @@
 import 'package:flutter/material.dart';
 
-Widget cardSection = Column(
-  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  children: [
-    _cardColumn(),
-    _cardColumn(),
-    _cardColumn(),
-  ],
-);
-
-Row _cardColumn() {
-  return Row(
-    mainAxisSize: MainAxisSize.min,
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Card(
-        margin: const EdgeInsets.all(10),
-        elevation: 5.5,
-        child: Column(
-          children: const [
-            ListTile(
-              title: Text('hd'),
-              subtitle: Text('The enourmes important subtitle'),
-              textColor: Colors.green,
-            )
-          ],
-        ),
-      ),
+Widget cardSection = Card(
+  margin: EdgeInsets.all(10),
+  elevation: 5.5,
+  child: Column(
+    children: const [
+      ListTile(
+        title: Text('Title'),
+        subtitle: Text('The enourmes important subtitle'),
+        textColor: Colors.green,
+      )
     ],
-  );
-}
+  ),
+);
